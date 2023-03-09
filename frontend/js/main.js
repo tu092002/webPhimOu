18c3ecc899119fb1541bc761a738579c69d734b3
tu <vongngocthank@gmail.com>
2023-03-09 17:28:43 +0700

    login, register

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,17 @@
 
 
+
+  window.onload = () =>{
+    let a = document.querySelector(".btnLogin");
+    let b = document.querySelector(".btnRegister");
+    a.onclick = () => {
+      window.location.assign('./login.html')
+    }
+    b.onclick = () => {
+      window.location.assign('./register.html')
+    }
+  }
+
   fetch('http://127.0.0.1:8000/film/')
     .then(response => response.json())
     .then(json => {
