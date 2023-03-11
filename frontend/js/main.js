37b8c45045512cfc07af06df39e658e2b8fc15d4
tu <vongngocthank@gmail.com>
2023-03-11 12:31:16 +0700

    ok

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,24 +1,69 @@
 
 
 
-  window.onload = () =>{
-    let a = document.querySelector(".btnLogin");
-    let b = document.querySelector(".btnRegister");
-    a.onclick = () => {
-      window.location.assign('./login.html')
-    }
-    b.onclick = () => {
-      window.location.assign('./register.html')
-    }
+window.onload = () => {
+  let a = document.querySelector(".btnLogin");
+  let b = document.querySelector(".btnRegister");
+  a.onclick = () => {
+    window.location.assign('./login.html')
+  }
+  b.onclick = () => {
+    window.location.assign('./register.html')
   }
 
-  fetch('http://127.0.0.1:8000/film/')
-    .then(response => response.json())
-    .then(json => {
-        json.forEach((item)=>{
-            console.log(item.name)
-        })
-    })
+  // header fix khi scroll sticky
+  const header = document.querySelector('header.header');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > header.offsetHeight) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  });
+
+
+
+
+}
+
+
+
+
+fetch('http://127.0.0.1:8000/film/')
+  .then(response => response.json())
+  // .then(json => {
+  //   json.forEach((item) => {
+  //     console.log(item.name)
+  //   })
+  // })
+  .then(movies => {
+
+    const container = document.querySelector('.movie-cards');
+
+    movies.forEach((movie) => {
+      console.log(movie)
+
+      const card = document.createElement('div');
+      card.classList.add('card1');
+
+      const image = document.createElement('img');
+      image.src = movie.image;
+      image.alt = movie.name;
+      card.appendChild(image);
+
+      const title = document.createElement('h2');
+      title.textContent = movie.name;
+      card.appendChild(title);
+
+      const description = document.createElement('p');
+      description.textContent = movie.description;
+      card.appendChild(description);
+
+      container.appendChild(card);
+    });
+
+  })
 
 
 
